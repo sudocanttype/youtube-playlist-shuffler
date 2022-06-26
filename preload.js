a65b11ba53ba2843b54f76279ed52ada207d2195
Oscar Lay <oscarlay789@gmail.com>
2022-06-26 12:17:27 -0500

    remove unneccesary files

--- preload.js
+++
@@ -1,7 +0,0 @@
-// All of the Node.js APIs are available in the preload process.
-// It has the same sandbox as a Chrome extension.
-const { contextBridge, ipcRenderer } = require('electron')
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  handleSubmit: (search) => ipcRenderer.invoke('submit_channel', search)
-})
